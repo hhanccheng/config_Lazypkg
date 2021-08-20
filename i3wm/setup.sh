59e9d29169d7cbd613964f8e98b926f1189fe63d
hhanccheng <hccao12o@gmail.com>
2021-08-20 12:12:13 -0400

    820211212

--- i3wm/setup.sh
+++ i3wm/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo pacman -S xfce4-terminal feh picom i3-gaps i3status rofi xfce4-input-synaptics
+#sudo pacman -S xfce4-terminal feh picom i3-gaps i3status rofi xfce4-input-synaptics
 cp i3.conf ~/.config/i3/config
 cp i3status.conf ~/.i3status.conf
 cp 70-synaptics.conf /etc/X11/xorg.conf.d/
