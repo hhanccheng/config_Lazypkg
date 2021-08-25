733a6945b836abfc6527fba8652a68d02a2b9ee6
hhanccheng <hccao12o@gmail.com>
2021-08-25 14:19:06 -0400

    82521219

--- archlinux/initsetup.sh
+++ archlinux/initsetup.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 #synaptics
-pacman -S xf86-input-synaptics
-cp 70-synaptics.conf /etc/X11/xorg.conf.d/
+sudo pacman -S xf86-input-synaptics
+sudo cp 70-synaptics.conf /etc/X11/xorg.conf.d/
 
 #yay
 git clone https://aur.archlinux.org/yay.git
